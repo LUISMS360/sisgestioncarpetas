812444d1e5afd05eba0989f389acf9803454736f
LUISMS360 <luisenrriquems0@gmail.com>
2025-11-23 11:30:55 -0500

    Actualizacion

--- app/Interfaces/FutRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Interfaces;
-
-interface FutRepositoryInterface
-{
-    public function searchByDni($dni);
-    public function createNew($data);
-}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Interfaces\FutRepositoryInterface;
 use App\Interfaces\UserRepositoryInterface;
-use App\Repositories\FutRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,7 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(UserRepositoryInterface::class,UserRepository::class);
-        $this->app->bind(FutRepositoryInterface::class,FutRepository::class);
     }
 
     /**
